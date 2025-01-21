42ecc3d413ba79bff8e82c753b4e31712c3a2d50
bgrmwbys <77823759+bgrmwbys@users.noreply.github.com>
2025-01-20 20:05:21 -0500

    Update Upgrade.bat

--- Upgrade.bat
+++ Upgrade.bat
@@ -1,3 +1,4 @@
 @echo off
 git pull && powershell -command "Stop-service -Force -name "RestorerRobot" -ErrorAction SilentlyContinue; go mod tidy; go build; Start-service -name "RestorerRobot""
 :: Hail Hydra
+pause
